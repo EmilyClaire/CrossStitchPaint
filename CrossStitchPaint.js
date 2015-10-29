e15cd306091739ac6da6e982a78d3662e06a1d7d
Emily Claire Gok Lucas <eclucas@calpoly.edu>
2015-10-29 16:21:36 -0700

    Commented out the base.js file

--- CrossStitchPaint.js
+++ CrossStitchPaint.js
@@ -1,3 +1,46 @@
+/* Setting the stitch variables */
+var stitchLength = 22;  //Actual length of the stitch
+var stitchThick = 6; //Thickness of the diagnal stitch
+
+//diagnal length of the square the stitch takes up
+var stitchDiag = stitchLength + stitchThick * 2 / Math.sqrt(2);
+//Length of the side (side === width === height) of the whole square the stitch takes up
+var stitchSide = stitchLength / Math.sqrt(2) + stitchThick;
+
+/*Setting up the base cloth grid that cannot be erased ever*/
+var canvasBase = document.getElementById('base');
+var base = canvasBase.getContext('2d');
+
+/*Making the background and outline box*/
+base.fillStyle = '#F5F6CE';
+base.fillRect(0, 0, canvasBase.width, canvasBase.height);
+
+/*Setting line width and color*/
+base.lineWidth = Math.sqrt(2) * stitchThick;
+base.strokeStyle = '#BDBDBD';
+
+/*A method that draws the grid background*/
+function drawAida(){
+
+    for(var i = 0; i * stitchSide + stitchSide < canvasBase.height - stitchSide; i ++){
+        base.beginPath();
+        base.moveTo(stitchSide + stitchSide * i, stitchSide);
+        base.lineTo(stitchSide + stitchSide * i, stitchSide);
+        base.lineTo(stitchSide + stitchSide * i, canvasBase.height - stitchSide);
+        base.closePath();
+        base.stroke();
+
+        base.beginPath();
+        base.moveTo(stitchSide, stitchSide + stitchSide * i);
+        base.lineTo(stitchSide, stitchSide + stitchSide * i);
+        base.lineTo(canvasBase.width - stitchSide, stitchSide + stitchSide * i);
+        base.closePath();
+        base.stroke();
+    }
+}
+
+drawAida();
+
 /*The canvas where the crossStitches go */
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
@@ -11,32 +54,33 @@ ctx.lineWidth = 1;
 
 /*drawing 19 stiches*/
 for(var i = 0; i < 19; i++){
-    rightCrossStitch(27 + i * 20, 20, 6, 20, 'red');
-    leftCrossStitch(27, 60 + 20 * i, 6, 20, 'blue');
-}
 
+    rightCrossStitch(2 * stitchSide + i * stitchSide,  2 * stitchSide, stitchThick, stitchLength, 'red');
+    leftCrossStitch(2 * stitchSide,  3 * stitchSide + stitchSide * i, stitchThick, stitchLength, 'blue');
+}
+/*
 for(var i = 0; i < 19; i++){
     rightCrossStitch(27 + i * 20, 40, 6, 20, 'green');
     leftCrossStitch(47, 60 + i * 20, 6, 20, 'orange');
 }
-
+*/
 /*Draws one diagnal stitch*/
 function drawDiagnalStitch(x,y,width,height,rad, color){
-    
+
     ctx.save();
-    
+
     /*Set the origin to the center of coordinates given*/
-    ctx.translate(x + width / 2, y + height / 2);
+    ctx.translate(x, y);
 
     /*Rotate the canvas*/
     ctx.rotate(rad * Math.PI);
 
     /*sets the color and draws and fills a rectangle*/
     ctx.fillStyle = color;
-    ctx.fillRect(width / -2, height / -2, width, height);
-    ctx.strokeRect(width / -2, height / -2, width, height);
-    
-    /*reset the canvas*/  
+    ctx.fillRect(-width / 2, -height / 2, width, height);
+    ctx.strokeRect(-width / 2, -height / 2, width, height);
+
+    /*reset the canvas*/
     ctx.restore();
 }
 
